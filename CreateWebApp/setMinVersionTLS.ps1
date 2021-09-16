c9ab724880cb00d8354e9b6f0660adf42a8afdf7
kbojjireddy <kbojjireddy@outlook.com>
2021-09-16 00:51:37 -0400

    Create setMinVersionTLS.ps1

---
+++ CreateWebApp/setMinVersionTLS.ps1
@@ -0,0 +1,13 @@
+param(
+  [string] [Parameter(Mandatory = $true)] $saName,
+  [string] [Parameter(Mandatory = $true)] $saResourceGroupName,
+  [string] [Parameter(Mandatory = $true)] $minimumTlsVersion
+)
+                                                              
+$ErrorActionPreference = 'Stop'
+$DeploymentScriptOutputs = @{}
+                                
+$output = 'Setting the MinimumTlsVersion to : {0} on the Storage Account : {1} located in the Resource Group : {2}.' -f $MinimumTlsVersion,$saName,$saResourceGroupName
+$DeploymentScriptOutputs['action1'] = $output
+Set-AzStorageAccount -ResourceGroupName $saResourceGroupName -AccountName $saName -MinimumTlsVersion $minimumTlsVersion
+$DeploymentScriptOutputs['end'] = 'End of the operation'
